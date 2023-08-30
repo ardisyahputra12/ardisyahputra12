d20f3b8d25efd5d622bfd1c3f02c9da6e4fed432
Ardi Syahputra <ardisaputraa12@gmail.com>
2023-08-30 13:44:44 +0700

    update skill data

--- src/data/skill.data.js
+++ src/data/skill.data.js
@@ -2,41 +2,41 @@ export const skills = {
   language: [
     {
       id: 1,
-      name: 'HTML',
-      color: '#FFF',
-      bg: '#E34F26'
-    },
-    {
-      id: 2,
-      name: 'CSS',
-      color: '#FFF',
-      bg: '#1572B6'
-    },
-    {
-      id: 3,
       name: 'JavaScript',
       color: '#000',
       bg: '#F7DF1E'
     },
     {
-      id: 4,
+      id: 2,
       name: 'Python',
       color: '#FFF',
       bg: '#14354C'
+    },
+    {
+      id: 3,
+      name: 'HTML',
+      color: '#FFF',
+      bg: '#E34F26'
+    },
+    {
+      id: 4,
+      name: 'CSS',
+      color: '#FFF',
+      bg: '#1572B6'
     },
   ],
   framework: [
     {
       id: 5,
-      name: 'Web Components',
-      color: '#FFF',
-      bg: '#323330'
+      name: 'ReactJs',
+      color: '#000',
+      bg: '#61DAFB'
     },
     {
       id: 6,
-      name: 'React',
-      color: '#000',
-      bg: '#61DAFB'
+      name: 'React Native',
+      color: '#FFF',
+      bg: '#323330'
     },
     {
       id: 7,
@@ -46,15 +46,15 @@ export const skills = {
     },
     {
       id: 8,
-      name: 'Flutter',
-      color: '#FFF',
-      bg: '#02569B'
+      name: 'Tailwind',
+      color: '#000',
+      bg: '#61DAFB'
     },
     {
       id: 9,
-      name: 'NodeJs',
+      name: 'Flutter',
       color: '#FFF',
-      bg: '#43853D'
+      bg: '#02569B'
     },
     {
       id: 10,
@@ -92,18 +92,30 @@ export const skills = {
   util: [
     {
       id: 15,
-      name: 'Docker',
+      name: 'WebpackJs',
       color: '#FFF',
       bg: '#0db7ed'
     },
     {
       id: 16,
+      name: 'Docker',
+      color: '#FFF',
+      bg: '#0db7ed'
+    },
+    {
+      id: 17,
       name: 'Git',
       color: '#FFF',
       bg: '#E44C30'
     },
     {
-      id: 17,
+      id: 18,
+      name: 'CI/CD',
+      color: '#FFF',
+      bg: '#404D59'
+    },
+    {
+      id: 19,
       name: 'Figma',
       color: '#FFF',
       bg: '#F24E1E'
